a28de1af518bc1cd3dada7d9f0e35d39f1817921
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2022-12-24 15:40:22 +0500

    push

--- components/agentPage/AgentFinallyComponent/index.jsx
+++ components/agentPage/AgentFinallyComponent/index.jsx
@@ -1,29 +1,36 @@
-import Container, {BoxCon, BtnCon} from './style'
-import {Button, Input} from "../../generic";
-import Orqaga from "../../../assets/icons/orqaga.svg";
-import {useRouter} from "next/router";
+import { useRouter } from 'next/router.js'
+import React, { useState } from 'react'
+import { Input } from '../../generic/Input/index.jsx'
+import Container, { AntModal, BoxCon, BtnCon, BtnCon1 } from './style.js'
+import Orqaga from "../../../assets/icons/orqaga.svg"
+import Button from "../../generic/Button";
+import Dowload from "../../../assets/mobile/icon/dowalond.svg"
 
-const AgentFinallyComponent = () => {
+export const AgentFinallyComponent = () => {
     const query = useRouter()
-    return(
+    return (
         <Container>
             <BoxCon>
-                <h1>
-                    Arizangiz muvaffaqqiyatli <br />
-                    qabul qilindi.
-                </h1>
+                <h1>Arizangiz
+                    muvaffaqiyatli
+                    </h1>
+                <h1> qabul qilindi.</h1>
                 <p>ID raqamingizni eslab qoling.</p>
-                <Input disabled={true} height={'61px'} width={'500px'} placeholder='№: 101004 ' />
+                <Input mwidth='100%' mheight={'45px'} mradius={'5px'} disabled={true} height={'61px'} width={'500px'} placeholder='№: 101004 ' />
                 <p>Parolingizni eslab qoling.</p>
-                <Input disabled={true} height={'61px'} width={'500px'} placeholder='AA2223344'  margin={'10px 0px 15px 0px'} />
-                <Button width={'344px'} height={'48px'} >yuklab olish</Button>
+                <Input mwidth='100%' mheight={'45px'} mradius={'5px'} disabled={true} height={'61px'} width={'500px'} placeholder='AA2223344' margin={'10px 0px 15px 0px'} />
+           
+                <BtnCon1>
+                    <Button mwidth='100%' width={'344px'} mheight={'45px'} msize={"20px"} mradius={'5px'} height={'48px'} >yuklab olish</Button>
+                    <Dowload className='dowlad' />
+                </BtnCon1>
+                 <BtnCon>
+                    <Button mwidth='100%' width={'267.47px'} mheight={'45px'} msize={"20px"} mradius={'5px'} onclick={() => query.push('/receptionPage')} className='btnEnd nocopy' height={"40px"} size={'22px'}>Ortga qaytish</Button>
+                    <Orqaga className='Orqaga' />
+                </BtnCon>
+
             </BoxCon>
-            <div></div>
-            <div></div>
-            <BtnCon>
-                <Button onclick={() => query.push('/receptionPage')} className='btnEnd nocopy' height={"40px"} width={'267.47px'} size={'22px'}>Ortga qaytish</Button>
-                <Orqaga className='Orqaga' />
-            </BtnCon>
+            
         </Container>
     )
 }
