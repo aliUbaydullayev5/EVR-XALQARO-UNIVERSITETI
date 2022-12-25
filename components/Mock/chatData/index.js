667ef93de80efbf1bd60892ff656cb3fdb8bbc9c
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2022-12-25 15:51:38 +0500

    done

---
+++ components/Mock/chatData/index.js
@@ -0,0 +1,213 @@
+import userAyol from "../../../assets/image/userAyol.png"
+import userDasturchi from "../../../assets/image/dasturchi.png"
+import user1 from "../../../assets/image/user1.png"
+import user2 from "../../../assets/image/user2.png"
+import user3 from "../../../assets/image/user3.png"
+
+
+  const ChatData = [
+    {
+        id: 1,
+        name: 'Elbek',
+        status: 'Software Engineer Developer ',
+        online: false,
+        notif: 11,
+        img:userDasturchi,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom 1',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum 2',
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Elbek',
+        status: 'Software Engineer Developer ',
+        online: false,
+        notif: 11,
+        img: userDasturchi,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'John Smith',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: userAyol,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Jenny',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: userAyol,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'User',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: user1,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: 'User',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: user2,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 7,
+        name: 'User',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: user3,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 8,
+        name: 'User',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: user1,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 9,
+        name: 'User',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: user1,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+    {
+        id: 10,
+        name: 'User',
+        status: 'Rektor',
+        online: false,
+        notif: 11,
+        img: user2,
+        messanger: [
+            {
+                id: 11,
+                by: 'admin',
+                messege: 'Salom',
+            },
+            {
+                id: 12,
+                by: 'client',
+                messege: 'Assalomu alekum',
+            }
+        ]
+    },
+
+
+
+  ]
+export default ChatData
--- components/chatPage/chatHom/index.jsx
+++ components/chatPage/chatHom/index.jsx
@@ -1,30 +1,72 @@
-import Container from "./style"
+import Container, { ChatAtvet, ChatCont, UserCon } from "./style"
 import {Button} from "../../generic";
 import {useRouter} from "next/router";
-
+import chatData from "../../Mock/chatData/index.js";
+import { useState } from "react";
+import Image from "next/image.js";
 
 const ChatComponents = () => {
     const router = useRouter()
+    const [data, setData] = useState(chatData)
+    
+    const [idChat, setIdChat] = useState(0)
+    const [message, setMessage] = useState([])
+
+    const findId = (id) => {
+        setIdChat(id)
+        const messageConst = data.filter((value) => value.id == id)[0]
+        setMessage(messageConst)
+    }
+
     return (
         <> 
          <Container.Top>
                 <Container.TopTitle>Shaxsiy xabarlar</Container.TopTitle>
           </Container.Top>
-        
-      
+
+            
         <Container>
                 <Container.Bottom>
                     <Container.BottomDesc>
                         <Container.BottomDescInset>
-                                    chat
+                            {data.map((val) => {
+                                return (
+                                    <UserCon onClick={() => findId(val.id)} key={val.id}>
+                                      <UserCon.Image>
+                                        <Image src={val.img} alt='user'/>
+                                        </UserCon.Image>
+                                        <UserCon.Box>
+                                         <UserCon.Name>
+                                            <h1>{val.name}</h1>
+                                            <p>{val.status}</p>
+                                            </UserCon.Name>
+                                          <UserCon.Notif>
+                                                <p>+{val.notif}</p>
+                                          </UserCon.Notif>
+                                     </UserCon.Box>
+                                    </UserCon> 
+                              )
+                          })}  
                         </Container.BottomDescInset>
                     </Container.BottomDesc>
                  </Container.Bottom>
-
                 <Container.Bottom>
-                   <Container.BottomDesc>
+                    <Container.BottomDesc>
                         <Container.BottomDescInset>
-                            chat
+                            <ChatCont>
+                                <Image alt={'img of person'} src={message?.img} />
+                              <ChatCont.Text>
+                                    <h1>{message?.name}</h1>
+                                    <p>{message.status}</p>
+                              </ChatCont.Text>
+                            </ChatCont>
+                            {message?.messanger?.map(({ id, by, messege }) => (
+                                <ChatAtvet.Main position={by}>
+                                    <ChatAtvet key={id}>
+                                        <h3>{by} : {messege}</h3>
+                                    </ChatAtvet>
+                                </ChatAtvet.Main>
+                            ))}
                         </Container.BottomDescInset>
                     </Container.BottomDesc>
                 </Container.Bottom>
