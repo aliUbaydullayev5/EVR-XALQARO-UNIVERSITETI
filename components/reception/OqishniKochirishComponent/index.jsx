af172fb6cf7518efea65e99cc6e6f0b934423743
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-01-11 11:09:32 +0500

    done

--- components/reception/OqishniKochirishComponent/index.jsx
+++ components/reception/OqishniKochirishComponent/index.jsx
@@ -6,31 +6,22 @@ import { useRouter } from "next/router.js";
 import { useEffect, useState } from "react";
 import UploadMobile from "../../../assets/mobile/icon/UploadMobile.svg"
 
+import { useDispatch, useSelector } from "react-redux";
+import deployFile, { deployFileFetch } from "../../../redux/slices/deployFile";
+import homeAllData, { homeAllDataFetch } from "../../../redux/slices/homeAllData";
+import { receptionPostFetch, resetVerify } from "../../../redux/slices/receptionPost";
+import { startMessage } from "../../../redux/slices/message";
 const OqishniKochirishComponent = () => {
     const router = useRouter()
+    const dispatch = useDispatch()
+
 
     const [numPasSeriya, setNumPasSeriya] = useState('')
     const [pasSerLength, setPasSerLength] = useState(0)
 
-    const changeMumPass = (event) => {
-        if (pasSerLength < event.length) {
-            setPasSerLength(event.length - 1)
-            if (event.length == 2) {
-                return setNumPasSeriya(event + ' ')
-            }
-            if (event.length == 6) {
-                return setNumPasSeriya(event + ' ')
-            }
-            if (event.length == 9) {
-                return setNumPasSeriya(event + ' ')
-            }
-        }
-        if (pasSerLength >= event.length) {
-            setPasSerLength(event.length)
-            setNumPasSeriya(event)
-        }
-        return setNumPasSeriya(event)
-    }
+
+    const [numState, setNumState] = useState('')
+    const [length, setLength] = useState(0)
 
     const [width, setWidth] = useState(null)
     useEffect(() => {
@@ -40,37 +31,7 @@ const OqishniKochirishComponent = () => {
             setWidth('513px')
         }
     })
-
-
-    const [numState, setNumState] = useState('')
-    const [length, setLength] = useState(0)
-
-    const changeNumState = (event) => {
-        if (length < event.length) {
-            setLength(event.length - 1)
-            if (event.length == 2) {
-                return setNumState(event + ' ')
-            }
-            if (event.length == 6) {
-                return setNumState(event + ' ')
-            }
-            if (event.length == 9) {
-                return setNumState(event + ' ')
-            }
-        }
-        if (length >= event.length) {
-            setLength(event.length)
-            setNumState(event)
-       
-        }
-      
- if (length.typeOf===Number) {
-     console.log(length.typeOf === Number);
- }
-        return setNumState(event)
-    }
-
-
+    
     const [numState1, setNumState1] = useState('')
     const [length1, setLength1] = useState(0)
 
@@ -91,7 +52,177 @@ const OqishniKochirishComponent = () => {
             setLength1(event.length)
             setNumState1(event)
         }
+        changeAllDataFunc({ type: 'extraPhoneNumber', value: event.split(' ').join('') })
         return setNumState1(event)
+    }
+
+
+    const changeNumState = (event) => {
+        if (length < event.length) {
+            setLength(event.length - 1)
+            if (event.length == 2) {
+                return setNumState(event + ' ')
+            }
+            if (event.length == 6) {
+                return setNumState(event + ' ')
+            }
+            if (event.length == 9) {
+                return setNumState(event + ' ')
+            }
+        }
+        if (length >= event.length) {
+            setLength(event.length)
+            setNumState(event)
+        }
+
+        changeAllDataFunc({ type: 'phoneNumber', value: event.split(' ').join('') })
+        return setNumState(event)
+    }
+
+
+
+
+
+    const changeMumPass = (event) => {
+        if (pasSerLength < event.length) {
+            setPasSerLength(event.length - 1)
+            if (event.length == 2) {
+                return setNumPasSeriya(event + ' ')
+            }
+            if (event.length == 6) {
+                return setNumPasSeriya(event + ' ')
+            }
+            if (event.length == 9) {
+                return setNumPasSeriya(event + ' ')
+            }
+        }
+        if (pasSerLength >= event.length) {
+            setPasSerLength(event.length)
+            setNumPasSeriya(event)
+        }
+        changeAllDataFunc({ type: 'passportSeries', value: event.split(' ').join('') })
+        return setNumPasSeriya(event)
+    }
+    const [allData, setAllData] = useState({
+        lastName: '',
+        admissionName: '',
+        firstName: '',
+        patron: '',
+        password: '',
+        prePassword: '',
+        passportSeries: '',
+        phoneNumber: '',
+        extraPhoneNumber: '',
+        courseLevel: 0,
+        studyLanguage: '',
+        educationType: '',
+        facultyId: 0,
+        diplomaId: '',
+        passportId: ''
+    })
+
+    useEffect(() => {
+        dispatch(homeAllDataFetch())
+    }, [])
+    const { educationTypes, faculties, studyLanguages } = useSelector((store) => store.homeAllData.data)
+    console.log(educationTypes,'sdds');
+    const changeAllDataFunc = ({ type, value }) => {
+        const fakeData = allData
+        fakeData[type] = value
+        setAllData(fakeData)
+        setAllData({ ...allData, [type]: value })
+    }
+    const { fileId, status, by } = useSelector((store) => store.deployFile)
+
+    useEffect(() => {
+        changeAllDataFunc({ type: by, value: fileId })
+    }, [fileId])
+
+    useEffect(() => {
+        changeAllDataFunc({ type: 'admissionName', value: 'BACHELOR' })
+    }, [])
+    const checkAllInputs = () => {
+        if (!(allData.lastName.length > 3)) {
+            dispatch(startMessage({ time: 5, message: 'Isim no togri kiritilgan' }))
+            return false
+        }
+        if (!(allData.firstName.length > 3)) {
+            dispatch(startMessage({ time: 5, message: 'Isim no togri kiritilgan' }))
+            return false
+        }
+        if (!(allData.patron.length > 3)) {
+            dispatch(startMessage({ time: 5, message: 'Isim no togri kiritilgan' }))
+            return false
+        }
+        if (!(allData.passportSeries.length == 9)) {
+            dispatch(startMessage({ time: 5, message: 'Passport seriya rakami no togri kiritilgan' }))
+            return false
+        }
+
+        if (!(allData.phoneNumber.length == 9)) {
+            dispatch(startMessage({ time: 5, message: 'Telefon raqamni togri kiritilgan' }))
+            return false
+        }
+        if (!(allData.extraPhoneNumber.length == 9)) {
+            dispatch(startMessage({ time: 5, message: 'Telefon raqamni togri kiritilgan' }))
+            return false
+        }
+        if (!(allData.diplomaId.length > 3)) {
+            dispatch(startMessage({ time: 5, message: 'Diplomingizni yulang' }))
+            return false
+        }
+        if (!(allData.passportId.length > 3)) {
+            dispatch(startMessage({ time: 5, message: 'Passportingizni yuklang' }))
+            return false
+        }
+        if (!(allData.password.length > 8)) {
+            dispatch(startMessage({ time: 5, message: 'Parolni kiriting' }))
+            return false
+        }
+        if (!(allData.prePassword.length > 8)) {
+            dispatch(startMessage({ time: 5, message: 'Parolni kiriting' }))
+            return false
+        }
+
+        if (!(allData.studyLanguage.length)) {
+            dispatch(startMessage({ time: 5, message: 'Oqish tilini tallang' }))
+            return false
+        }
+        if (!(allData.facultyId)) {
+            dispatch(startMessage({ time: 5, message: 'Oqish turini tallang' }))
+            return false
+        }
+        if (!(allData.educationType.length)) {
+            dispatch(startMessage({ time: 5, message: 'Oqish shaklini tallang' }))
+            return false
+        }
+        return true
+    }
+
+    const pushAllInfo = () => {
+        if (checkAllInputs()) {
+            dispatch(receptionPostFetch(allData))
+        }
+        console.log(allData)
+    }
+
+    const receptionData = useSelector((store) => store.receptionPost)
+
+    useEffect(() => {
+        if (receptionData?.status === 'error') {
+            dispatch(startMessage({ time: 5, type: 'error', message: receptionData.message }))
+        }
+    }, [receptionData])
+
+
+    if (receptionData.pushAnswer) {
+        router.push('/receptionPage/application/UsersCardInfo')
+        if (receptionData.status === 'success') {
+            dispatch(startMessage({ time: 5, type: 'success', message: receptionData.message }))
+        }
+        setTimeout(() => {
+            dispatch(resetVerify())
+        }, 2000)
     }
 
     return (
