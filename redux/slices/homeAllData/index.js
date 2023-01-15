cc90025d3e35eaa9598d5c17dc20451160a851c2
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-01-15 11:16:58 +0500

    update

--- redux/slices/homeAllData/index.js
+++ redux/slices/homeAllData/index.js
@@ -1,17 +1,7 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
 export const homeAllDataFetch = createAsyncThunk('homeAllData', async (payload)=> {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
     return await fetch('http://192.168.122.61:1010/api/v1/home').then((res)=> res.json())
-
-=======
-    return await fetch('http://192.168.122.61:1010/api/v1/home').then((res)=> res.json())
->>>>>>> main
-=======
-    return await fetch('http://192.168.122.61:1010/api/v1/home').then((res)=> res.json())
->>>>>>> main
 })
 
 const homeAllData = createSlice({
@@ -26,19 +16,7 @@ const homeAllData = createSlice({
         },
         [homeAllDataFetch.fulfilled]: (state, action)=> {
             state.status = 'success'
-<<<<<<< HEAD
-            if(action?.payload?.success == true){
-
-                state.data = action?.payload?.data
-<<<<<<< HEAD
-                console.log(action?.payload)
-
-=======
->>>>>>> main
-            }
-=======
             if(action?.payload?.success == true) state.data = action?.payload?.data
->>>>>>> main
         },
         [homeAllDataFetch.rejected]: (state)=> {
             state.status = 'error'
