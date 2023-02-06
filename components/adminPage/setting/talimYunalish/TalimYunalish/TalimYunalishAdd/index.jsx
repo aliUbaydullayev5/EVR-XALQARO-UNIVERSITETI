a63f5db00eda0413a4e1769ba6191bbf64c542fa
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-02-06 16:00:32 +0500

    done

--- components/adminPage/setting/talimYunalish/TalimYunalish/TalimYunalishAdd/index.jsx
+++ components/adminPage/setting/talimYunalish/TalimYunalish/TalimYunalishAdd/index.jsx
@@ -4,113 +4,76 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { getStudyTypesFetch } from '../../../../../../redux/slices/getStudyTypes/index.jsx'
 import Input from '../../../../../generic/Input/index.jsx'
-import Container from './style.js'
-
-const data = [
-    {
-        id: 1,
-        name:'Boshlang’ich ta’lim'
-    },
-    {
-        id: 2,
-        name: 'Maktabgacha ta’lim'
-    },
-    {
-        id: 3,
-        name: 'Iqtisodiyot(Tarmoqlar va sohalar bo’yicha)'
-    },
-    {
-        id: 4,
-        name: 'Yo’l harakatini tashkil etish'
-    },
-    {
-        id: 5,
-        name: 'Yurisprudensiya(Mehnat huquqi)'
-    },
-    {
-        id: 6,
-        name: 'Filologiya va tillarni o’qitish(tillar bo’yicha)'
-    },
-    {
-        id: 7,
-        name: 'Ахборот тизимлари ва технологиялари(Тармоқлар ва соҳалар бўйича)'
-    },
-    {
-        id: 8,
-        name: 'Тарих(Мамлакатлар ва йўналишлар бўйича)'
-    },
-    {
-        id: 9,
-        name: 'Архитектура(турлар бўйича)'
-    },
-    {
-        id: 10,
-        name: 'Банк иши'
-    },
-]
+import DataAriza from '../../../../../Mock/adminAriza/data.js'
+import Container, { ConTable } from './style.js'
 
 
 export const TalimYunlishAddCom = () => {
     const dispatch = useDispatch()
 
-    const [datas, Setdatas] = useState(data);
-    const [adduser, Setadduser] = useState()
-    const [funok, Setfunkok] = useState(adduser)
+    const [data, setData] = useState(DataAriza)
+    const [selectAllState, setSelectAllState] = useState(false)
 
-    const handleDelete = (id) => {
-        Setdatas(datas.filter((val) => val.id  !== id));
-    }
+
     useEffect(() => {
-        console.log(dispatch(getStudyTypesFetch({ type: 'Talim Yunalish Qoshish Abuturent' })));
+        setData(data.map((value) => (
+            {
+                id: value.id,
+                ismi: value.ismi,
+                phone: value.phone,
+                data: value.data,
+                img: value.img,
+                checked: selectAllState
+            }
+        )))
+    }, [selectAllState])
+
+
+    const selectOne = (id = false) => {
+        setData(data.map((value) => (
+            {
+                id: value.id,
+                ismi: value.ismi,
+                phone: value.phone,
+                data: value.data,
+                img: value.img,
+                checked: value.id === id ? !value.checked : value.checked
+            }
+        )))
+    }
+
+
+    useEffect(() => {
+       dispatch(getStudyTypesFetch({ type: 'Talim Yunalish Qoshish Abuturent' }));
 
     }, [])
 
-
-    const onChange = (e, id) => {
-        const {name,value}= e.target
-  
-        const editData = () => {
-       return datas.map((item) => item.id === id && name ? { ...item, [name]: value } : item)
-        }
-       Setdatas(editData)
-    }
-    const addFunc = (e,) => Setadduser(e.target.value)
-
-    const addFuncOk = () => {
-        Setfunkok(adduser)
-}
     return (
         <Container>
-            <Container.BottomDesc>
-                <Container.BottomDescInset>
-            <Container.Table>
-                <Container.Input>
-                    <div>
-                                <Input onchange={(e) => addFunc(e)} width={'290px'} height={'35px'} radius={'5px'} padding={'0px 10px '} size={'20px'} type="text" placeholder={'Talim Yunalishlar Qoshish'}  /></div>
-                            <div><button onClick={addFuncOk } className='buttonAdd'>add</button></div>
-                </Container.Input>
-                <br/>
-                <tbody style={{ width: "100%" }}>
-                        <tr>
-                        <th className='thid'>Id</th>
-                        <th>Ta'lim Yunlishlar Nomi</th>
-                        <th>Delte</th>
-                        </tr>
-                            {datas?.map(({name,id}) => { 
+            <Container.Scrool style={{ overflowY: 'scroll', maxHeight: '550px' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', }}>
+                    <Container.Nav>
+                        <div className='row'>
+                            <div >№</div>
+                            <div className='colum'>Talim Yunalish Turlari</div>
+                            <div className='colum'>Action</div>
+                        </div>
+                    </Container.Nav>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', }}>
+                    {data.map((value) => {
                         return (
-                            <tr key={id}>
-                                <td className='thid'>{id}</td>
-                                <td className='thname'>
-                                    <td>{funok}</td>
-                                    <Input width={'100%'} height={'40px'} radius={'5px'} size={'20px'} padding={'0px 10px'} name="name" value={name} type="text" placeholder="Type Name" onchange={(e) => onChange(e, id)} />
-                                </td>
-                                <td><button className='buttonDelete' onClick={()=>handleDelete(id)}>delete</button></td>
-                             </tr>   ) })}
-                        </tbody>
-             
-                    </Container.Table>
-                </Container.BottomDescInset>
-            </Container.BottomDesc>
+                            <ConTable key={value.id}>
+                                <div className='row'>
+                                    <div >{value.id}</div>
+                                    <div className='colum'>{value.ismi}</div>
+                                    <div className='colum'>{value.phone}</div>
+                                </div>
+                            </ConTable>
+                        )
+                    })}
+                </div>
+            </Container.Scrool>
         </Container>
     )
 }
--- components/adminPage/setting/talimYunalish/TalimYunalish/TalimYunalishAdd/style.js
+++ components/adminPage/setting/talimYunalish/TalimYunalish/TalimYunalishAdd/style.js
@@ -1,133 +1,108 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 const Container = styled.div`
   width: 100%;
+  margin: 0 auto;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  display: grid;
+  max-width: 1440px;
+  grid-template-columns: 5fr 0.7fr;
+  padding: 0px 40px;
+ @media only screen and (max-width: 1000px) {
+   padding: 0px 20px;
+  }
 
 `
-Container.Input = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    .buttonAdd{
-    background-color: #21d05b;
-    color: white;
-    border: none;
-    cursor: pointer;
-    width: 45px;
-    height: 30px;
-    border-radius: 5px;
+Container.Scrool = styled.div`
+   width: 100%;
+   height: 615px !important;
+   overflow-y: scroll;
+   padding: 0 15px 0 0;
+   height: 100%;
+   display: grid;
+   ::-webkit-scrollbar {
+      width: 5px;
+    }
+    ::-webkit-scrollbar-track {
+      width: 10px !important;
+      padding: 0 5px 0 0;
+      background: #9a9a9a;
+      opacity: 0.5;
+      border-radius: 30px;
+      transform: matrix(-1, 0, 0, 1, 0, 0);
+      transform: matrix(-1, 0, 0, 1, 0, 0);
+      padding: 10px !important;
+    }
 
+    ::-webkit-scrollbar-thumb {
+      background: #FFFFFF;
+      border-radius: 30px;
+      transform: matrix(-1, 0, 0, 1, 0, 0);
     }
 `
+const ConTable = styled.div`
+  display: grid;
+  grid-template-columns:  1fr;
+  gap:10px;
+  >div{
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #FFFFFF;
+  }
+  .row{
+    display: grid;
+    grid-template-columns: 50px 10fr  200px;
+    padding-left: 15px;
+    background: linear-gradient(90deg, #1E3C72 0%, #1E3C72 1%, #2A5298 100%);
+    border-radius: 5px;
+  }
+    box-shadow: 0px 2px 17px rgba(0, 0, 0, 0.54);
+    border-radius: 5px;
+  .colum{
+    border-left:  2px solid white;
+    padding-left: 10px;
+  }
 
-Container.Table = styled.div`
-max-width: 670px;
-
-  >tbody {
-   min-height: 400px;
-   >tr{
-      border: 1px solid #D9D9D9 ; 
-      
-      
-      th{
-      color: white;
-      border: 1px solid #D9D9D9;;
-        padding: 7px;
-
-      
-      }
-      >td{
-      border: 1px solid #D9D9D9;;
-      height:35px;
-      color: white;
-        padding: 7px;
-
-      .buttonEdit{
-        border: none;
-        width: 45px;
-        height: 30px;
-        cursor: pointer;
-        background-color: #2185d0;
-        border-radius: 5px;
-        color: white;
-
-      }
-      .buttonDelete{
-      background-color: #f51010;
-      color: white;
-      border: none;
-      cursor: pointer;
-      width: 45px;
-      height: 30px;
-      border-radius: 5px;
-
-      }
-      }
-      .thid{
-        width: 45px;
-        height: 45px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        padding: 7px;
-
-      }
-      .thname{
-       width: 550px;
-      }
-   }
-}
 `
-Container.BottomDesc = styled.div`
-  height: 550px !important;
-  border: 1px solid #FFFFFF;
-  height: 100%;
-  padding: 15px;
-  @media only screen and (max-width: 1000px) {
-    height: 550px !important;
+Container.Nav = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 10px;
+  margin-bottom: 10px;
+
+  >div{
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #FFFFFF;
   }
-`
-Container.BottomDescInset = styled.div`
-  width: 100%;
-  height: 530px !important;
-  overflow-y: scroll;
-  padding: 0 10px 0 0;
-
-  @media only screen and (max-width: 1000px) {
-    height: 530px !important;
+  .row{
+    display: grid;
+    grid-template-columns:  50px 10fr  200px;
+    padding-left: 15px;
+    background: linear-gradient(90deg, #1E3C72 0%, #1E3C72 1%, #2A5298 100%);
+    border-radius: 5px;
   }
-  ::-webkit-scrollbar {
-    width: 5px;
+    box-shadow: 0px 2px 17px rgba(0, 0, 0, 0.54);
+    border-radius: 5px;
+  .colum{
+    border-left:  2px solid white;
+    padding-left: 10px;
   }
-
-
-  ::-webkit-scrollbar-track {
-    width: 10px !important;
-    padding: 0 5px 0 0;
-    background: #9a9a9a;
-    opacity: 0.5;
-    border-radius: 30px;
-    transform: matrix(-1, 0, 0, 1, 0, 0);
-    transform: matrix(-1, 0, 0, 1, 0, 0);
-    padding: 10px !important;
-
-  }
-
-  /* Handle */
-
-  ::-webkit-scrollbar-thumb {
-    background: #FFFFFF;
-    border-radius: 30px;
-    transform: matrix(-1, 0, 0, 1, 0, 0);
-  }
-
-  /* Handle on hover */
 
 `
 
 
+export { ConTable }
 export default Container
