0fbf17bb37b93053a38563794b77d15c002c426b
ali ubaydullayev <mukhammadali.ubaydullayev.05@inbox.ru>
2023-02-28 11:28:55 +0500

    update

--- redux/slices/firstVerify/index.js
+++ redux/slices/firstVerify/index.js
@@ -1,7 +1,7 @@
 
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 export const firstVerifyFetch = createAsyncThunk('postFirstVerify', async (payload)=> {
-    console.log(payload)
+    console.log(payload, 'check-phone3')
     return await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'https://evredu.uz/api/'}v1/auth/check-phone`, {
         method: 'POST',
         headers: {
