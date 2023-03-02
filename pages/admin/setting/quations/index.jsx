2aa13e0de031925231e1d34eed8d87f219db485a
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-03-02 16:24:06 +0500

    update

--- pages/admin/setting/quations/index.jsx
+++ pages/admin/setting/quations/index.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import AdminRootToken from '../../../../components/adminPage/outlet/index.jsx'
 import RootAdmin from '../../../../components/adminPage/RootLogo/index.jsx'
 import RootSidebar from "../../../../components/adminPage/RootSidebarMenu/index.jsx"
-import QuationCom from '../../../../components/adminPage/setting/testCom/Quation/index.jsx'
+import TableTest from '../../../../components/adminPage/setting/testCom/QuationResaltTeset/index.jsx'
+// import QuationCom from '../../../../components/adminPage/setting/testCom/Quation/index.jsx'
 
 export const Menagement = () => {
   return (
     <RootAdmin>
       <RootSidebar>
         <AdminRootToken>
-          <QuationCom/>
+          {/* <QuationCom/> */}
+          <TableTest/>
         </AdminRootToken>
       </RootSidebar>
     </RootAdmin>
