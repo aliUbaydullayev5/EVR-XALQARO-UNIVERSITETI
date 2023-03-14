d5944568d34ce482db66770699d8f5857e130c2d
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-03-14 21:21:19 +0500

    done

--- components/adminPage/setting/talimYunalishType/TalimYunalishTypeAdd/style.js
+++ components/adminPage/setting/talimYunalishType/TalimYunalishTypeAdd/style.js
@@ -190,8 +190,53 @@ Container.ButtonLoader = styled.div`
   background: rgba(0, 0, 0, 0.05);
   border-radius: 4px;
 `
+const AntSelect = styled(Select)`
+ .ant-select-dropdown, .css-dev-only-do-not-override-1s3dcof, .ant-select-dropdown-placement-topLeft{
+   background-color: transparent !important;
+  }
+  .ant-select-selection-item{
+    color: #fff !important;
+    display: flex;
+    align-items: center;
+    font-size: 18px !important;
+    font-weight: 400 !important;
+    @media only screen and (max-width: 1000px) {
+      font-size: 16px !important;
+      padding: 0 12px !important;
+    }
+  }
+  .ant-select-arrow{
+    color: white !important;
+    font-size: 20px;
+    @media only screen and (max-width: 1000px) {
+      font-size: 13px;
+    }
+  }
+  .ant-select-selection-search-input{
+    display: none !important;
+  }
+  .ant-select-selection-placeholder{
+    display: flex;
+    align-items: center;
+    font-size: 22px !important;
+    color: #ffff;
+    @media only screen and (max-width: 800px) {
+      font-size: 16px !important;
+    }
+  }
 
-export { ConTable, Antmodal }
+ .ant-select-item-option-content{
+  font-size: 17px !important;
+ } 
+ .ant-select-item .ant-select-item-option .ant-select-item-option-active{
+   font-size: 17px !important;
+ }
+ .ant-select-selector {
+  border-radius: 0 !important;
+}
+`
+
+export { ConTable, Antmodal ,AntSelect}
 export default Container
 
 
