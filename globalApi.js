49dfd413de705983cc2315f7ea8a061f257ff90d
ali ubaydullayev <mukhammadali.ubaydullayev.05@inbox.ru>
2023-05-21 13:24:48 +0500

    update

--- globalApi.js
+++ globalApi.js
@@ -1,1 +1,1 @@
-export const API_GLOBAL = process.env.NEXT_PUBLIC_BASE_URL || 'http://evruniversity.uz/api/'
+export const API_GLOBAL = process.env.NEXT_PUBLIC_BASE_URL || 'http://213.230.69.57:8088/api/'
